da60ad7b0bfecdf762c0423000aec543bad870b3
kontopro <konteap@gmail.com>
2023-04-02 21:02:29 +0300

    subcategories

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -3,10 +3,11 @@ import Image from "next/image";
 import { supabase } from "@/lib/supabaseClient"
 import styles from '@/styles/Category.module.css' 
 
-export default function Category( { category } ) {
-
-    return <main className={styles.main}>
+export default function Category( { category, subcategories, kyria } ) {
+    
+    return  <main className={styles.main}>
                 <div>You are in category <b>{category.name}!</b></div>
+                <div>{}</div>
             </main>
 } 
 
@@ -21,9 +22,17 @@ export async function getStaticPaths(){
 
 export async function getStaticProps( { params } ){
 
-    const {data: menu, error} = await supabase.from('category').select().eq('slug',params.category)
+    // Τσίμπα την κατηγορία από το slug
+    const {data: menu, error: err1} = await supabase.from('category').select().eq('slug',params.category)
     const category = menu[0]
+    // Έλεγχος για sub-categories
+    const {data: submenu, error: err2} = await supabase.from('category').select().eq('parent_id',category.id)
+    const subcategories = submenu
+    console.log(subcategories)
+    // Έλεγχος για κύρια υλικά της κατηγορίας
+    const {data: kyria, error: err3} = await supabase.from('kyrio').select().eq('category_id',category.id)
+    console.log(kyria)
     return {
-        props: {category}
+        props: {category, subcategories, kyria}
     }
 }
