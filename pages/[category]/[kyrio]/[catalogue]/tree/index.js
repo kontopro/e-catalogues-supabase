5af03d82fd04b4b757dad1e8d221b5ee29771dc1
kontopro <konteap@gmail.com>
2023-04-09 17:16:59 +0300

    tree

---
+++ pages/[category]/[kyrio]/[catalogue]/tree/index.js
@@ -0,0 +1,9 @@
+
+
+export default function Tree(){
+    return <>
+            <p>hello <b>Tree navigation</b> like old catalogues</p>
+            <p>For each catalogue: I want <b>all</b> assemblies under their parent assemblies</p>
+            <p>+ menu for modern (card) navigation</p>
+            </>
+}
