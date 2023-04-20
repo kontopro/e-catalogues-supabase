cb8a8e4839158db13004a8db3e234f32c01f7533
kontopro <konteap@gmail.com>
2023-04-20 22:15:32 +0300

    css
    card

--- components/Mayout.js
+++ components/Mayout.js
@@ -5,6 +5,7 @@ function Mayout( {children} ){
 
     return (<div className="site-app">
                 <header className="header">
+                    <div className="top-header"></div>
                     <p>header here</p>
                 </header>
                 <div className="container">
