12cb662db1616872b6e25e4db2d368ec865088ea
kontopro <konteap@gmail.com>
2023-04-26 13:16:11 +0300

    tree
    new paths

--- pages/[category]/[kyrio]/[catalogue]/tree/index.js
+++ pages/[category]/[kyrio]/[catalogue]/tree/index.js
@@ -1,29 +1,51 @@
 import { supabase } from "@/lib/supabaseClient"
-import Assembly from "../[assembly]"
+import styles from '@/styles/Tree.module.css' 
 
 
-export default function Tree(){
-    return  <>
-                <div>
-                <p>hello <b>Tree navigation</b> like old catalogues</p>
-                <p>For each catalogue: I want <b>all</b> assemblies under their parent assemblies</p>
-                <p>+ menu for modern (card) navigation</p>
-                </div>
-                {/* Χωρισμός στα δύο τμήματα: tree και image με ΑΟ */}
-                <div className="tree-container">
-                        <div className="tree">
-                            <p>Εδώ η δενδρική μορφή</p>
-                            <div className="treeass"></div>
-                        </div>
-                        <div className="imgnsn">Εδώ η εικόνα του υποσυγκορτήματος κι από κάτω οι ΑΟ που περιέχονται σε αυτή!</div>
-                </div>   
-            </>
+export default function Tree( { parent_assemblies } ) {
+    return (
+      <main className={styles.main}>
+        <div>
+          <p>You are in test directory</p>
+          {/* <p>{menu.map(x => <div key={x.id}>{x.id}</div>)}</p> */}
+          <p>hello </p>
+          <p>I now test the tree module </p>
+          <div className='tree-container'>
+          <div className="tree">
+           {/* <pre>{JSON.stringify(menus, null, 2)}</pre> */}
+           {parent_assemblies.map(parent => <div key={parent.id} className='parent-container'>
+                                                                 
+                                  <h4>{parent.name}</h4>
+                                  <div className='sub-container'>{parent.assembly.map(child_assembly => <div key={child_assembly.id} className='subassembly'>{child_assembly.name}</div>)}</div>
+                              </div>
+            )
+            }
+            </div>
+            <div className='imgnsn'>listnsn</div>
+            </div>
+        </div>
+      </main>
+    )
+  }
+
+export async function getStaticPaths(){
+    
+    // inner join ώστε να κουμπώσει κάθε κύριο με την κατηγορία του και τους καταλόγους του
+    const {data: catalogues, error} = await supabase.from('catalogue').select('slug,kyrio:kyrio_id (slug,category:category_id (slug) )')
+    
+    const paths = catalogues.map(x => ({params: {category: x.kyrio.category.slug, kyrio: x.kyrio.slug, catalogue: x.slug}}))
+    return {paths, fallback: false}
 }
-   export async function getStaticProps (){
 
-        const {data, error} = await supabase.from('catalogue').select('id,name,slug').eq('slug','cat-chassis-leo1a5')
-        const catalogue = data;
-        console.log(catalogue)
-        return null
-
-   }     
+export async function getStaticProps( { params } )  {
+  
+  
+    const {data: catalogue, error1} = await supabase.from('catalogue').select('id,name,slug').eq('slug',params.catalogue)  
+    const {data: parent_assemblies, error2} = await supabase.from('assembly').select('id,name,caption,assembly (id,name,caption)').eq('catalogue_id',catalogue[0].id).is('parent_id',null)
+        
+    return {
+      props: {
+        parent_assemblies,
+      }
+    }    
+  } 
