ed999386f4ae8030f1b8d9636416bfaea2eb5f62
kontopro <konteap@gmail.com>
2023-06-19 10:43:55 +0300

    aithsh
    without css

--- pages/[category]/[kyrio]/aithsh/index.js
+++ pages/[category]/[kyrio]/aithsh/index.js
@@ -1,7 +1,76 @@
+import { useAithsh } from "@/components/AithshState";
+
 export default function Aithsh(){
+
+    const {aithsh, handleAithsh} = useAithsh();
+
     return (
         <>
-        <h1>this is AITHSH !!</h1>
-        </>
+      
+      <main className='aithsh'>
+        <div className='print'>
+          <div className='print-title'>
+            <h1>Αίτηση Μηχανογραφικώς Χορηγουμένων Υλικών</h1>
+          </div> 
+          <table className='t1'>
+            <tbody>
+            <tr><td>ΑΙΤΟΥΣΑ ΜΟΝΑΔΑ</td><td>ΜΟΝΑΔΑ ΜΕΣΩ</td><td>ΔΓΗ ΧΟΡΗΓΗΣΗΣ</td><td>ΔΓΗ ΔΕΣΜΕΥΣΗΣ</td></tr>  
+            <tr><td></td><td>Η'</td><td></td><td></td></tr>  
+            <tr><td>ΧΟΡΗΓΟΥΣΑ ΜΟΝΑΔΑ</td><td>ΜΟΝΑΔΑ ΔΙΑ</td><td>ΚΩΔΙΚΑΣ ΔΓΗΣ</td><td>ΚΩΔΙΚΑΣ ΔΓΗΣ</td></tr>  
+            <tr><td></td><td></td><td></td><td></td></tr>  
+            </tbody>
+          </table>       
+          <table className='t2'>
+          <thead>
+            <tr>
+              <th>ΚΩΔΙΚΑΣ ΕΝΤΥΠ.</th>
+              <th>ΑΡΙΘΜΟΣ ΟΝΟΜΑΣΤΙΚΟΥ</th>
+              <th>PART NUMBER</th>
+              <th>ΟΝΟΜΑΣΙΑ</th>
+              <th>ΚΥΡΙΟ ΥΛΙΚΟ</th>
+              <th>ΑΙΤΟΥΜΕΝΗ ΠΟΣΟΤΗΤΑ</th>
+              <th>ΤΑΥΤΟΤΗΤΑ ΕΝΤΥΠΟΥ</th>
+              <th>ΜΜ</th>
+              <th>ΑΙΤΙΟΛΟΓΙΑ</th>
+              <th>ΠΡΟΤΕΡΑΙΟΤΗΤΑ</th>
+              <th>ΔΕΛΤΙΟ</th>
+              <th>ΠΑΡΑΤΗΡΗΣΕΙΣ</th>
+            </tr>
+          </thead>
+          {aithsh.length===0?<tbody>
+            <tr><td colSpan='12'>Δεν έχουν καταχωρηθεί υλικά στην αίτηση</td></tr>
+            
+          </tbody>:
+          <tbody>    
+          {aithsh.map(x=>
+            <tr key={x.ao}>
+              <td> </td>
+              <td>{x.ao}</td>
+              <td> </td>
+              <td>{x.per}</td>
+              <td> </td>
+              <td>{x.pos}</td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              {/* <td><Barcode width={1} height={30} displayValue={false} value={`${x.ao} ${x.pos}`} /> </td> */}
+            </tr>
+          )}
+          </tbody>} 
+          <tfoot>
+            <tr>
+            <td colSpan='3' rowSpan='3'>ΥΠΟΓΡΑΦΗ ΑΙΤΟΥΝΤΟΣ ΚΑΙ ΣΦΡΑΓΙΔΑ ΜΟΝΑΔΑΣ</td>
+            <td colSpan='3' rowSpan='3'>ΕΓΚΡΙΝΟΝΤΟΣ ΤΗΝ ΑΙΤΗΣΗ ΚΑΙ ΟΙΚΕΙΑ ΣΦΡΑΓΙΔΑ</td>
+            <td colSpan='3' rowSpan='3'>ΥΠΟΓΡΑΦΗ ΚΩΔΙΚΟΠΟΙΗΣΗΣ ΥΠΑΛΛΗΛΟΥ</td>
+            <td colSpan='3' rowSpan='3'>Υποβάλλεται υπό τύπου αναφοράς με τη Φ.600.14/.................................................................</td>
+            </tr>
+          </tfoot>
+          </table>
+        </div>
+      </main>
+    </>
     )
 }
