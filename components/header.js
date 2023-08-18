8821eb53b84afd6208ae8b53138531cd949d3b36
kontopro <konteap@gmail.com>
2023-08-18 12:43:07 +0300

    main nav menu

--- components/header.js
+++ components/header.js
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Header(){
     return(
             <header className="header">
@@ -6,7 +8,21 @@ export default function Header(){
                         <div className="top-header-center"><p>e-Catalogues</p></div>
                         <div className="top-header-right"><p>2023</p></div>
                     </div>
-                    <p>navigation here</p>
+                    <div className="nav-header">
+                        <nav className="main-nav">
+                            <ul>
+                                <li><Link href="/home">Αρχική</Link></li>
+                                <li><Link href="/oximata">Οχήματα</Link></li>
+                                <li><Link href="/armata">Άρματα</Link></li>
+                                <li><Link href="/oplismos">Οπλισμός</Link></li>
+                                <li><Link href="/pirovola">Πυροβόλα</Link></li>
+                                <li><Link href="/sa">Σ/Α</Link></li>
+                                <li><Link href="/dioptres">Διόπτρες</Link></li>
+                                <li><Link href="/genika">Γενικά Υλικά</Link></li>
+                                <li><Link href="/search">Αναζήτηση</Link></li>
+                            </ul>
+                        </nav>
+                    </div>
             </header>
     )
 }
