9f1e2e3b86b4f7262d3bea0ccb9225537b16e249
kontopro <konteap@gmail.com>
2023-10-13 14:11:00 +0300

    Εκτύπωση

--- components/Printbutton.js
+++ components/Printbutton.js
@@ -6,7 +6,7 @@ export default function Printbutton(){
 
     return(
         <div className='right-sub-nav'>
-            <button type="button" onClick={() => window.print()}>Εκύπωση Αίτησης</button>
+            <button type="button" onClick={() => window.print()}>Εκτύπωση Αίτησης</button>
         </div>
     )
 }
