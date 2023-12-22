f5dc432a387c7a34e9863da581f99b855fc49d9b
kontopro <konteap@gmail.com>
2023-12-22 16:17:47 +0200

    double

--- pages/[category]/[kyrio]/index.js
+++ pages/[category]/[kyrio]/index.js
@@ -12,7 +12,7 @@ export default function Kyrio( { category, kyrio, ecats } ) {
                 <Subnav printbtn={false} aithshbtn={true} txt={`Διαθέσιμοι Ηλεκτρονικοί Κατάλογοι για το ΚΥ: ${kyrio.name}`} />                
                 <div className='cards-wrapper'>{ecats.map(x => <div key={x.id} className="card">
                                 <div className='card-image'>
-                                  <p><Image src={`${basePath}/images/category/${kyrio.slug}.jpg`} alt={`catalogue-${x.slug}`} width='400' height='200'/></p>
+                                  <p><Image src={`${basePath}/images/catalogue/${x.slug}/000.jpg`} alt={`catalogue-${x.slug}`} width='400' height='200'/></p>
                                 </div>
                                 <div className='card-title'>                             
                                   <p className="no-mobile"><Link href={`/${category}/${kyrio.slug}/${x.slug}/tree`}>{x.name}</Link></p>
